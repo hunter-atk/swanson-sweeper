6ad248b1f383b4ff40b138f8a35d4a28c2cdd666
Hunter Jefferson <hj@artkade.com>
2021-04-07 16:40:57 -0500

    Adds simple GET request with Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+export const App: React.FC = () => {
+  const [scores, setScores] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://glacial-eyrie-20134.herokuapp.com/api/scores/intermediate`)
+      .then(res => {
+        const result = res.data;
+        setScores(result);
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {JSON.stringify(scores)}
     </div>
   );
 }
